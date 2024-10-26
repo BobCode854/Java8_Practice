c2763d8d8711eb5509780ddddc5c74f9706b9969
Karan Sinha <35830975+BobCode854@users.noreply.github.com>
2024-10-26 20:17:41 +0530

    Delete practice/src/practice directory

--- practice/src/practice/array/primitive/Primitive.java
+++
@@ -1,9 +0,0 @@
-package practice.array.primitive;
-public class Primitive {
-
-	public static void main(String[] args) {
-		System.out.println("Primitive");
-
-	}
-
-}
--- practice/src/practice/set/primitive/Primitive.java
+++
@@ -1,9 +0,0 @@
-package practice.set.primitive;
-public class Primitive {
-
-	public static void main(String[] args) {
-		System.out.println("Primitive");
-
-	}
-
-}
