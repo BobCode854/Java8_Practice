020c10888ad6e70640fbd5f73bbaf3810a856536
Karan Sinha <35830975+BobCode854@users.noreply.github.com>
2024-11-15 08:51:25 +0530

    Create Java8Code.java

---
+++ practice/src/Collection_Using_LambdaExpression/Java8Code.java
@@ -0,0 +1,34 @@
+package com.java8.Collection_Using_LambdaExpression;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+/*
+ *      Discuss : 
+ *      --------------
+ *      Operation  : Sorting ( ascending order )
+ *      Default Behavior : Insertion order .
+ *      Functional Interface : Comparator class
+ *      method : compare().
+ *      
+ *     We will remove an extra MyComparator class as we can directly
+ *      write lambda expression for my compare method.
+ *   
+ */
+public class Java8Code {
+	public static void main(String args[]) {
+		ArrayList<Integer> l = new ArrayList<>();
+		l.add(20);
+		l.add(10);
+		l.add(25);
+		l.add(5);
+		l.add(30);
+		l.add(0);
+		l.add(15);
+		System.out.println("Before sorting : " + l);
+
+		Comparator<Integer> c = (n1, n2) -> (n1 > n2) ? +1 : (n1 < n2) ? -1 : 0;
+		Collections.sort(l, c);
+		System.out.println("After Sorting : " + l);
+	}
+}
